60f50b4ac22e6095d748a396d6c63c7b60720c84
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-04 19:54:49 +0530

    Update mochatest.js

--- mochatest.js
+++ mochatest.js
@@ -1,9 +1,24 @@
-var calc = require("./calc.js");
 var assert = require('assert');
+var calc = require("./calc.js");
+
 
 describe("calc test",function(){
-	it("returns 4+5=9",function(done){
-		assert.equal(calc.add(4,5), 9);
+	 it("returns 4+5=9",function(done){
+	 	assert.equal(calc.add(4,5), 9);
+   
 		done();
-	})
+	});
+
+it("returns 4*5=20",function(done){
+	 	assert.equal(calc.mul(4,5), 20);
+   
+		done();
+	});
+
+it("returns 5-4=1",function(done){
+	 	assert.equal(calc.sub(5,4), 1);
+   
+		done();
+	});
+	
 });

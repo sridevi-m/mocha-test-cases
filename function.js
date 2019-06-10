1f84bbc88b21b61b194841d8bb00c40781fce4dd
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-10 15:24:51 +0530

    Update function.js

--- function.js
+++ function.js
@@ -9,3 +9,19 @@ exports.mul = function(i,j){
 exports.sub = function(i,j){
 	return i-j;
 };
+
+exports.div = function(i,j){
+	return i/j;
+};
+
+exports.fun = function(x,y){
+	if(x==y){
+		return "x and y are equal"
+	}else{
+		return "x and y are not equal"
+	}
+};
+ 
+exports.fun_one = function(){
+	return "securifi emebedded systems"
+};

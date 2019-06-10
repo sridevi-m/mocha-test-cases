1606255235134392bb64cfd367603002bcab3a7a
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-10 17:44:21 +0530

    Update mochatest.js

--- mochatest.js
+++ mochatest.js
@@ -3,39 +3,36 @@ var calc = require("./calc.js");
 
 
 describe("calc test",function(){
+	this.timeout(10000)
 	 it("returns 4+5=9",function(done){
 	 	assert.equal(calc.add(4,5), 9);
-   
-		done();
+		setTimeout(done,2000);
 	});
 
 it("returns 4*5=20",function(done){
 	 	assert.equal(calc.mul(4,5), 20);
-   
-		done();
+		setTimeout(done,2000);
 	});
 
 it("returns 5-4=1",function(done){
 	 	assert.equal(calc.sub(5,4), 1);
-   
-		done();
+		setTimeout(done,2000);
 	});
-	
-});
 
 it("returns 10/2 ==5",function(done){
 	assert.equal(calc.div(10,2), 5)
-	done();
+	setTimeout(done,2000);
    });
 
-it("fun checking",function(){
+it("x and y are equal",function(done){
 	assert.equal(calc.fun(19,19),"x and y are equal")
+	    	setTimeout(done,2000);
     });
 
-it("function",function(){
+it("securifi emebedded systems",function(done){
 	assert.equal(calc.fun_one(), "securifi emebedded systems")
-   });
-
+		setTimeout(done,2000);
+});
 });
 
 describe('Array', function() {
@@ -46,17 +43,17 @@ describe('Array', function() {
   });
 });
 
-describe('Array', function() {
+
+
+describe('Array', function() {  
   describe('#indexOf()', function() {
     // pending test below
     it('should return -1 when the value is not present');
   });
 });
 
-
 describe('a suite of tests', function() {
    this.timeout(9000);
-
   it('should take less than 500ms', function(done) {
   	assert.equal(calc.div(10,2), 5)
     setTimeout(done, 2000);

67cb479f703d0a5378a4a5b388cd312b397ad5b7
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 20:24:59 +0530

    Create multiplefunction.js

---
+++ multiplefunction.js
@@ -0,0 +1,17 @@
+var fun1 = function(msg1){
+	console.log(msg1);
+};
+
+
+var fun2 = function(msg2){
+	console.log(msg2);
+};
+
+var fun3 = function(msg3){
+	console.log(msg3);
+};
+
+var fun4 = function(x,y){
+    console.log(x*y);
+}
+module.exports = {fun1, fun2, fun3, fun4};

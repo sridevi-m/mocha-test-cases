89f05245e3be6b7c00b0de30f1710f6b58dedbea
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 18:01:23 +0530

    Create imports.js

---
+++ imports.js
@@ -0,0 +1,12 @@
+var msg = require("./variables.js");
+console.log(msg);
+
+var msg = require("./variables.js");
+console.log(msg.simpleMsg);
+
+var msg = require("./variables.js");
+msg.funct("hello india")
+
+
+var msg  = require("./variables.js");
+console.log(msg.fName+ " "+ msg.tName);

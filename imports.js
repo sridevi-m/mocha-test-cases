e2d2168c39caba806c168bd4223818b1635c5dc1
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 19:25:36 +0530

    Update imports.js

--- imports.js
+++ imports.js
@@ -35,3 +35,8 @@ console.log(msg.obj2.fName);
 // multiple objects calling
 var msg = require("./variables.js");
 console.log(msg.obj4.fName)
+
+// importing multiple values
+
+var values = require("./variables.js");
+console.log(values.value1+values.value2+values.value3+values.value4+values.value4);

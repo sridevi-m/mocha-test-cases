00ae92ea7aba4956b1e04fd5fb919e03c2823009
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 17:57:51 +0530

    Create exports.js

---
+++ exports.js
@@ -0,0 +1,16 @@
+module.exports = "securifi embedded systems";
+
+
+module.exports.simpleMsg = "secruifi embeded systems at madhapur";
+
+# funct
+  module.exports.funct = function(msg){
+ 	console.log(msg);
+  };
+  
+# object
+ module.exports = {
+ 	fName : "gowrav",
+ 	tName : "rahul"
+ }
+ 

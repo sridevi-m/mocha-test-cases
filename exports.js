9a0a6d71fb488f8417833b4b0dd6f060ff9e5b1c
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 19:24:58 +0530

    Update exports.js

--- exports.js
+++ exports.js
@@ -88,4 +88,14 @@ exports.fun4 = function(x,y){
     console.log(x*y);
 }
 
+// multiple variables exporting
+
+var value1 = 200;
+var value2 = 250;
+var value3 = 500;
+var value4 = 1000;
+var value5 = 999;
+
+module.exports = {value1, value2, value3, value4, value5};
+
 

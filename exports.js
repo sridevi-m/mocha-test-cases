3d09d636ad046f44a368613ba61c2d15c96219d8
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 18:33:09 +0530

    Update exports.js

--- exports.js
+++ exports.js
@@ -21,3 +21,23 @@ module.exports.simpleMsg = "secruifi embeded systems at madhapur";
  	this.desgination = desgination;
  	this.gender = gender;
  }
+
+
+//multiple funcitons
+
+exports.fun1 = function(msg1){
+	console.log(msg1);
+};
+
+
+exports.fun2 = function(msg2){
+	console.log(msg2);
+};
+
+exports.fun3 = function(msg3){
+	console.log(msg3);
+};
+
+exports.fun4 = function(x,y){
+    console.log(x*y);
+}

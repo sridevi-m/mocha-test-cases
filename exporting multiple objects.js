d897ad3a30d89e55c3dfdc13ca6334fb745cc9ba
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 20:28:44 +0530

    Create exporting multiple objects.js

---
+++ exporting multiple objects.js
@@ -0,0 +1,20 @@
+var obj1= {
+	fName : "gowrav",
+     tName : "rahul"
+}
+
+var obj2 = {
+	fName : "swaroop",
+	tName : "pentakota"
+}
+
+var obj3 = {
+	fName : "rudra",
+	tName : "jagadeswari"
+}
+
+var obj4 ={
+   fName : "pulsar",
+   tName : "suzuki"
+}
+module.exports = {obj1,obj2,obj3,obj4};

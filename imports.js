a2f35f99b5d5261282fc4e6aa0c1fa161583467e
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 18:11:18 +0530

    Update imports.js

--- imports.js
+++ imports.js
@@ -10,3 +10,8 @@ msg.funct("hello india")
 
 var msg  = require("./variables.js");
 console.log(msg.fName+ " "+ msg.tName);
+
+
+var msg = require("./variables.js");
+var msg2 = new msg("gowrav",23, "node js developer", "male");
+console.log(msg2.name+ " "+ msg2.age+ " "+msg2.desgination+ " "+msg2.gender);

cecfc533d0da34797bb4c931a37325cba6314f36
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-12 19:09:37 +0530

    Update exports.js

--- exports.js
+++ exports.js
@@ -37,7 +37,32 @@ exports.fun2 = function(msg2){
 exports.fun3 = function(msg3){
 	console.log(msg3);
 };
+  
 
+                          // (or)
+
+
+// multiple objects
+var obj1= {
+	fName : "gowrav",
+     tName : "rahul"
+}
+
+var obj2 = {
+	fName : "swaroop",
+	tName : "pentakota"
+}
+
+var obj3 = {
+	fName : "rudra",
+	tName : "jagadeswari"
+}
+
+var obj4 ={
+   fName : "pulsar",
+   tName : "suzuki"
+}
+module.exports = {obj1,obj2,obj3,obj4};
 // multiple objects
 exports.obj1 = {
 	fName : "gowrav",
